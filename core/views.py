de1245600586cf1a928f43c1d9e24a75845bccd0
Hiago Fagundes Couto <hiago.couto@hotmail.com>
2020-03-24 19:40:30 -0300

    Creating event listing screen

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Event
 
 # Create your views here.
+
+def index(request):
+    user = request.user
+    events = Event.objects.filter(user=user)
+    data = {
+            'events': events,
+            'user': user,
+            }
+    return render(request, 'schedule.html', data)
